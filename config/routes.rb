c00fb8d07d3da3430eba7c6b359c76fdf474a228
umay.oit07@gmail.com <177226299+m-oit@users.noreply.github.com>
2024-09-13 08:28:51 +0000

    [中断-9/13]bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'books/new'
-  get 'books/show'
-  get 'books/edit'
-  get 'books' => 'homes#top'
-  post 'books' => 'books#create'
-  get 'books' => 'boooks#index'
+  get 'books' => 'books#index'
+  get '/' => 'homes#top'
+  post 'books' => 'books#create' 
+  get 'books/:id' => 'books#show', as: :book
+  get 'books/:id/edit' => 'books#edit',as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
